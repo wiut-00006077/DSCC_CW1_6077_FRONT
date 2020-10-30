10602db380274e811ff1209b0c11199e45534bb8
wiut-00006077 <lennon645@gmail.com>
2020-10-30 11:05:36 +0500

    My First Commit

---
+++ DSCC_CW1_6077_FRONT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSCC_CW1_6077_FRONT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
